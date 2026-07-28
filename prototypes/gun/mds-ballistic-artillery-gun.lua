67f2987d6f7a5191b7eb9daa9b89ad9932255c5e
Em Karisch <emii.khaos@gmail.com>
2026-07-28 06:57:43 +0200

    fix: don't output artillery shell casings

--- prototypes/gun/mds-ballistic-artillery-gun.lua
+++ prototypes/gun/mds-ballistic-artillery-gun.lua
@@ -8,7 +8,8 @@ khaoslib_item.copy("gun", "artillery-wagon-cannon", "mds-ballistic-artillery-gun
       cooldown = math.max(1, 60 / settings.startup["mds-ballistic-shots-per-second"].value),
       range = settings.startup["mds-ballistic-max-range"].value --[[@as integer]],
       min_range = settings.startup["mds-ballistic-minimum-range"].value --[[@as integer]],
-      sound = {{filename = "__base__/sound/fight/rocket-launcher.ogg", volume = 0.75}}
+      sound = {{filename = "__base__/sound/fight/rocket-launcher.ogg", volume = 0.75}},
     }
   }
+  :unset("attack_parameters.shell_particle")
   :commit()
